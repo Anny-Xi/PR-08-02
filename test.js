3d9201f18c873a426f127461e7fc8ff1cdd7c41d
AnnyXi <114316992+Anny-Xi@users.noreply.github.com>
2024-04-07 21:02:37 +0200

    back up ?

--- test.js
+++ test.js
@@ -24,6 +24,56 @@ document.addEventListener('keydown', (event) => {
 });
 
 
+    // halen localstorage met dat label
+    let hands = JSON.parse(localStorage.getItem(itemName));
+
+    // als er geen data is, terug naar functie
+    if (hands.length === 0) {
+        return;
+    }
+    // push dat naar het data met label
+    for (const hand of hands) {
+        data.push({ pose: hand, label: direction })
+    }
+
+
+
+
+// localstorage hand datas bewaren van bijbehorende lable
+function saveHandler(direction) {
+    let startTimeMs = performance.now();
+    const results = handLandmarker.detectForVideo(video, startTimeMs);
+
+    if (results.landmarks.length === 0) {
+        return;
+    }
+    console.log(results.landmarks[0]);
+    let savedHands = [];
+    for (const markPosition of results.landmarks[0]) {
+        // console.log(`x position ${markPosition.x} y position ${markPosition.y}`);
+        savedHands.push(markPosition.x, + markPosition.y, +markPosition.z);
+    }
+    // lege tempArray = []
+    let handArray = []
+    // localstorage ophalen
+    // als leeg
+    // als niet leeg
+    if (localStorage.getItem(direction)) {
+        // push naar tempArray
+        // omzetten naar array
+        handArray = JSON.parse(localStorage.getItem(direction));
+    }
+
+    // push naar array
+    handArray.push(savedHands);
+    // omzetten naar json
+    console.log(handArray)
+    const handJson = JSON.stringify(handArray);
+
+    // localstorage setItem
+    localStorage.setItem(direction, handJson);
+
+}
 
 // neurol network aanmaken
 const nn = ml5.neuralNetwork({ task: 'classification', debug: true });
